cbbfc859588d53d35ace9de053b8c11aa8095dcb
Nikhith <nikhinikhithc@gmail.com>
2026-06-21 20:18:40 +0530

    nikhith

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IdentifierService } from './common/services';
 import { CanonicalService } from './common/services/canonical.service';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Meta } from '@angular/platform-browser';
-import { filter, map } from 'rxjs';
 import { GoasosTitleService } from './common/services/title.service';
 import { MetaService } from './common/services/meta.service';
 import { AnalyticsService } from './common/services/analytics.service';
@@ -14,31 +11,22 @@ import { VideoDialogService } from './common/services/video-dialog.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [IdentifierService, 
-    CanonicalService, 
+  providers: [IdentifierService,
+    CanonicalService,
     GoasosTitleService,
     MetaService,
     AnalyticsService,
-  UserAnalyticsEvents]
+    UserAnalyticsEvents]
 })
 export class AppComponent {
   title = 'goasos';
+
   constructor(
     private ser: IdentifierService,
     private link: CanonicalService,
     private video: VideoDialogService
   ) {
     link.AddLink();
-  }
-
-  @ViewChild('men') private menuDiv!: ElementRef<HTMLDivElement>
-
-  public ShowSideMenu(): void {
-    this.menuDiv.nativeElement.classList.add('show');
-  }
-
-  public HideSideMenu(): void {
-    this.menuDiv.nativeElement.classList.remove('show');
   }
 
   getRouteAnimationData() {
--- src/app/common/components/hero/hero.component.ts
+++ src/app/common/components/hero/hero.component.ts
@@ -4,10 +4,12 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 export class HeroComponent {
   @Input({ required: true }) public showCLient!: boolean;
   @Input({ required: true }) public h1!: string;
   @Input({ required: true }) public p!: string;
+  @Input() public premium = false;
+  @Input() public backgroundImage = 'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?auto=format&fit=crop&w=1920&q=80';
 }
